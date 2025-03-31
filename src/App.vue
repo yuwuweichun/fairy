9b3f9da88dd7de74e556a15fd3afcc79650ef10c
SongChun <yuwuweichun522@gmail.com>
2025-03-31 23:06:27 +0800

    better text

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@ import { prompt } from './system'
 /**
  * output
  */
-const guidingText = "系统启动完成——我是Ⅲ型总序式集成泛用人工智能，开发代号Fairy。\n你好，主人！(主人，按下ctrl+space以唤出Fairy，输入文本后按下enter向Fairy发送消息)"
-const rousingText = "你好，主人，有什么Fairy可以帮你"
-const loadingText = "Fairy正在以五倍耗电模式思考\nloading...................................................................................................\n主人，你是不是忘记交电费了"
+const guidingText = "系统启动完成——我是Ⅲ型总序式集成泛用人工智能，开发代号Fairy。你好，主人！\n(主人，按下ctrl+space以唤出Fairy，输入文本后按下enter向Fairy发送消息)"
+const rousingText = "你好，主人，有什么Fairy可以帮你?"
+const loadingText = "Fairy正在以五倍耗电模式思考\nloading...................................................................................................\n主人，你是不是忘记交电费了，切换为正常耗电模式\nloading..................................................................................................."
 let output = ref("")
 output.value = guidingText
 
